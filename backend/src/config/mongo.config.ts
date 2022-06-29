aad03191a644bb33a322cbfe7bb6c05f8f70125c
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-06-29 17:48:06 -0400

    created mongodb configs

--- backend/src/config/mongo.config.ts
+++ backend/src/config/mongo.config.ts
@@ -4,18 +4,20 @@ dotenv.config();
 const MONGO_OPTIONS = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  socketTimeoutMS: 30000,
+  socketTimeoutMS: 10000,
   keepAlive: true,
-  poolSize: 50,
   autoIndex: false,
   retryWrites: false
 };
 
 const MONGO_PORT = parseInt(process.env.MONGO_PORT || "27017");
 const MONGO_HOST = process.env.MONGO_HOST || "localhost";
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "AlkitabDB";
 
+const MONGO = {
+  host: MONGO_HOST,
+  options: MONGO_OPTIONS,
+  url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`
+};
 
-
-const dbConfig = {};
-
-export default dbConfig;
+export default MONGO;
