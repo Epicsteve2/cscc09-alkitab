41d0ac31892f845e781f22d158f546a83e2f1a2a
Stephen Guo <epicsteveoj@gmail.com>
2022-07-04 23:27:44 -0400

    [frontend] Add navbar and footer

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,28 +2,77 @@
   import Home from "./routes/Home.svelte";
   import TestPage from "./routes/TestPage.svelte";
 
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Container,
+    Row,
+    Col,
+  } from "sveltestrap/src";
+
   // svelte-routing
   import { Router, Link, Route } from "svelte-routing";
   export let url = "";
+
+  let isOpen: boolean = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
 </script>
 
 <main>
   <Router {url}>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="test">Test</Link>
-    </nav>
+    <Navbar color="warning" light expand="md">
+      <!-- <Container> Idk this container is weird, TODO: Fix-->
+      <NavbarBrand href="/">Alkitab 📚</NavbarBrand>
+      <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+      <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav navbar class="me-auto">
+          <NavItem class="px-2">
+            <Link to="/" class="nav-link">Bookshelf</Link>
+          </NavItem>
+          <NavItem class="px-2">
+            <Link to="/add-books" class="nav-link">Add books</Link>
+          </NavItem>
+          <NavItem class="px-2">
+            <Link to="/podcasts" class="nav-link">Podcasts</Link>
+          </NavItem>
+        </Nav>
+        <Nav navbar>
+          <NavItem class="px-2">
+            <NavLink href="/login-signup">Login/Signup</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+      <!-- </Container> -->
+    </Navbar>
     <div>
       <Route path="test" component={TestPage} />
       <Route path="/" component={Home} />
     </div>
+    <footer>
+      <Navbar color="light">
+        <Nav class="mx-auto">
+          <Link to="credits" class="px-3 nav-link">Credits</Link>
+          <a
+            class="px-3 nav-link"
+            href="https://github.com/UTSCC09/project-we-use-linux-btw">GitHub</a
+          >
+        </Nav>
+      </Navbar>
+    </footer>
   </Router>
 </main>
 
 <style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
 </style>
