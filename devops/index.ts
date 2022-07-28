358cf421ec30812c17c156c1c015dfb455694260
Stephen Guo <epicsteveoj@gmail.com>
2022-07-28 17:22:28 -0400

    Get frontend working in k8s

--- devops/index.ts
+++ devops/index.ts
@@ -1,40 +1,37 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
+import * as nginx from "@pulumi/kubernetes-ingress-nginx";
+import * as docker from "@pulumi/docker";
 
-// // Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
-// // running on minikube, and if so, create only services of type ClusterIP.
-// const config = new pulumi.Config();
-// const isMinikube = config.requireBoolean("isMinikube");
+const stack = pulumi.getStack();
 
-// const appName = "nginx";
-// const appLabels = { app: appName };
-// const deployment = new k8s.apps.v1.Deployment(appName, {
-//   spec: {
-//     selector: { matchLabels: appLabels },
-//     replicas: 1,
-//     template: {
-//       metadata: { labels: appLabels },
-//       spec: { containers: [{ name: appName, image: "nginx" }] },
-//     },
-//   },
-// });
+const config = new pulumi.Config();
+const isMinikube = config.requireBoolean("isMinikube");
 
-// // Allocate an IP to the Deployment.
-// const frontend = new k8s.core.v1.Service(appName, {
-//   metadata: { labels: deployment.spec.template.metadata.labels },
-//   spec: {
-//     type: isMinikube ? "ClusterIP" : "LoadBalancer",
-//     ports: [{ port: 80, targetPort: 80, protocol: "TCP" }],
-//     selector: appLabels,
-//   },
-// });
+// const ns = new k8s.core.v1.Namespace("default");
 
-// // When "done", this will print the public IP.
-// export const ip = isMinikube
-//   ? frontend.spec.clusterIP
-//   : frontend.status.loadBalancer.apply(
-//       (lb) => lb.ingress[0].ip || lb.ingress[0].hostname
-//     );
+const appName = "alkitab";
+const appLabels = { app: appName };
+
+// Minikube does it's own thing with an ingress controller
+if (!isMinikube) {
+  const nginxIngressController = new nginx.IngressController(
+    "nginx-ingress-controller",
+    {
+      controller: {
+        publishService: {
+          enabled: true,
+        },
+        service: {
+          type: isMinikube ? "ClusterIP" : "LoadBalancer",
+        },
+      },
+      // helmOptions: {
+      //     namespace: ns.metadata.name,
+      // },
+    }
+  );
+}
 
 const mongodb = new k8s.helm.v3.Chart("mongodb", {
   chart: "mongodb",
@@ -56,51 +53,135 @@ const mongodb = new k8s.helm.v3.Chart("mongodb", {
   },
 });
 
-const mongoExpress = new k8s.helm.v3.Chart(
-  "mongo-express",
-  {
-    chart: "mongo-express",
-    version: "2.6.6",
-    fetchOpts: {
-      repo: "https://cowboysysop.github.io/charts/",
-    },
-    values: {
-      image: {
-        tag: "1.0.0-alpha.4",
-      },
-      mongodbEnableAdmin: true,
-      mongodbAuthUsername: "root",
-      mongodbAuthPassword: "123456",
-      siteBaseUrl: "/mongo-express",
-      // TODO: connect to database
-    },
-  },
-  { dependsOn: mongodb.ready }
-);
+// const mongoExpress = new k8s.helm.v3.Chart(
+//   "mongo-express",
+//   {
+//     chart: "mongo-express",
+//     version: "2.6.6",
+//     fetchOpts: {
+//       repo: "https://cowboysysop.github.io/charts/",
+//     },
+//     values: {
+//       image: {
+//         tag: "1.0.0-alpha.4",
+//       },
+//       mongodbEnableAdmin: true,
+//       mongodbAuthUsername: "root",
+//       mongodbAuthPassword: "123456",
+//       siteBaseUrl: "/mongo-express",
+//       // TODO: connect to database
+//     },
+//   },
+//   { dependsOn: mongodb.ready }
+// );
 
-const nginx = new k8s.helm.v3.Chart(
-  "nginx",
-  {
-    chart: "nginx",
-    version: "13.1.4",
-    fetchOpts: {
-      repo: "https://charts.bitnami.com/bitnami",
-    },
-    values: {
-      image: {
-        tag: "1.23.1-debian-11-r2",
-      },
-      containerPorts: {
-        https: 0, // TODO
-      },
-      service: {
-        type: "Ingress Controller",
-      },
-      ingress: {
-        enabled: true,
-        selfSigned: false, // TODO
+const whoami = new k8s.helm.v3.Chart("whoami", {
+  chart: "whoami",
+  fetchOpts: {
+    repo: "https://cowboysysop.github.io/charts/",
+  },
+});
+
+const frontendDeployment = new k8s.apps.v1.Deployment(appName, {
+  spec: {
+    selector: { matchLabels: appLabels },
+    replicas: 1,
+    template: {
+      metadata: { labels: appLabels },
+      spec: {
+        containers: [
+          {
+            name: `${appName}-frontend`,
+            image: "alkitab-frontend",
+            imagePullPolicy: "Never",
+            ports: [
+              {
+                containerPort: 80,
+              },
+            ],
+          },
+        ],
       },
     },
   },
-  { dependsOn: mongodb.ready }
-);
+});
+
+const frontendService = new k8s.core.v1.Service(appName, {
+  metadata: {
+    labels: frontendDeployment.spec.template.metadata.labels,
+    name: "alkitab-frontend",
+  },
+  spec: {
+    // type: isMinikube ? "ClusterIP" : "LoadBalancer",
+    type: "ClusterIP",
+    ports: [{ port: 80 }],
+    selector: appLabels,
+  },
+});
+
+// Next, expose the app using an Ingress.
+const appIngress = new k8s.networking.v1.Ingress(`alkitab-ingress`, {
+  metadata: {
+    name: "test-ingress2",
+    annotations: {
+      "kubernetes.io/ingress.class": "nginx",
+      "nginx.ingress.kubernetes.io/rewrite-target": "/$1",
+      // "ingress.kubernetes.io/configuration-snippet": "try_files $uri $uri/ /index.html",
+    },
+    // namespace: ns.metadata.name
+  },
+  spec: {
+    defaultBackend: {
+      service: {
+        // name: frontendDeployment.metadata.name,
+        name: "alkitab-frontend",
+        port: { number: 80 },
+      },
+    },
+    rules: [
+      {
+        // Replace this with your own domain!
+        // host: "myservicea.foo.org",
+        http: {
+          paths: [
+            {
+              pathType: "Prefix",
+              path: "/test3",
+              backend: {
+                service: {
+                  name: "whoami",
+                  port: { number: 80 },
+                },
+              },
+            },
+            {
+              pathType: "Prefix",
+              path: "/(.*)",
+              backend: {
+                service: {
+                  // name: frontendDeployment.metadata.name,
+                  name: "alkitab-frontend",
+                  port: { number: 80 },
+                },
+              },
+            },
+            {
+              pathType: "Prefix",
+              path: "/testagain",
+              backend: {
+                service: {
+                  // name: frontendDeployment.metadata.name,
+                  name: "alkitab-frontend",
+                  port: { number: 80 },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+});
+
+// export const controllerStatus: pulumi.Output<any> =
+//   nginxIngressController.status;
