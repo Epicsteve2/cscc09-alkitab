bfda79c10850b0a76d10d43a8b160c2b3e350b19
Andy Yuen <andyren.yuen@mail.utoronto.ca>
2022-07-28 15:55:45 -0400

    routes for bookposts

--- backend/src/controllers/bookPost.ts
+++ backend/src/controllers/bookPost.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { RequestHandler } from 'express/ts4.0';
+import IBookPost from '../interfaces/bookPost';
 import BookPost from '../models/bookPost';
 
 const NAMESPACE = 'BookPost Controller';
@@ -11,5 +12,26 @@ export const makeNewPost: RequestHandler = async (req: Request, res: Response, n
     })
 
     newPost.save();
+
+    res.status(200).json({newPosting: newPost});
+}
+
+export const incrOwners: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id
+    const bookPost: any = await BookPost.findById(id);
+
+    bookPost.numberOfOwners++;
+    bookPost.save();
+
+    res.status(200).json({posting: bookPost});
+
+}
+
+export const getBookPosts: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const limit = req.query.limit ? Number(req.query.limit) : 5;
+
+    const posts = await BookPost.find({}).sort({bookName: 'asc'}).limit(limit);
+
+    res.status(200).json({posts: posts});
 }
 
