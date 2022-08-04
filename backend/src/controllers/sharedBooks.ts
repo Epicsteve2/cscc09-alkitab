5dcb9d4acbb85588bf12ed851a7bb0e2c24ded28
Stephen Guo <epicsteveoj@gmail.com>
2022-08-03 20:31:03 -0400

    Fix sharing book twice

--- backend/src/controllers/sharedBooks.ts
+++ backend/src/controllers/sharedBooks.ts
@@ -7,39 +7,35 @@ import { userExists } from '../util/user';
 const NAMESPACE = 'SharedBooks Controller';
 
 export const addShareRelationship: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    if (! await userExists(req.body.sharee)) 
-        res.status(404).json({msg: "User dose not exist"})
+	if (!(await userExists(req.body.sharee))) res.status(404).json({ msg: 'User dose not exist' });
+	else {
+		const existingShare = await BookShare.findOne({ $and: [{ bookId: req.body.bookId }, { sharer: req.session.user }, { sharee: req.body.sharee }] });
 
-    
-    else {
-        const existingShare = await BookShare.find({$and : [{bookId : req.body.bookId}, {sharer: req.session.user}, {sharee:req.body.sharee}]})
-        if (existingShare){
-            res.status(400).json({msg: "Already added"})
-        } else {
-            const newShare = new BookShare({
-                bookId: req.body.bookId,
-                sharer: req.session.user || req.body.sharer,
-                sharee: req.body.sharee
-            });
-        
-            newShare.save();
-        
-            res.status(200).json({ msg: 'relation added' });
-        }
-    }
+		if (existingShare) {
+			res.status(400).json({ msg: 'Already added' });
+		} else {
+			const newShare = new BookShare({
+				bookId: req.body.bookId,
+				sharer: req.session.user || req.body.sharer,
+				sharee: req.body.sharee
+			});
+
+			newShare.save();
+
+			res.status(200).json({ msg: 'relation added' });
+		}
+	}
 };
 
 export const getSharedBooks: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    const limit = req.query.limit ? Number(req.query.limit) : 5;
-    const username = req.session.user
+	const limit = req.query.limit ? Number(req.query.limit) : 5;
+	const username = req.session.user;
 
-    const sharedRelations = await BookShare.find({sharee: username}).select('bookId').limit(limit);
-    const bookIds = sharedRelations.map(realtion => realtion.bookId);
-    const books = await Book.find({ _id : { $in : bookIds }}).select('-pages').limit(limit);
+	const sharedRelations = await BookShare.find({ sharee: username }).select('bookId').limit(limit);
+	const bookIds = sharedRelations.map((realtion) => realtion.bookId);
+	const books = await Book.find({ _id: { $in: bookIds } })
+		.select('-pages')
+		.limit(limit);
 
-    return res.status(200).json({books : books})
-
-
-}
-
-
+	return res.status(200).json({ books: books });
+};
